9c2cc626e3f5b5cfd1d07232dda8f8c53f92b13c
xingtongli <32304597+xingtongli@users.noreply.github.com>
2021-11-05 02:03:34 -0400

    Add files via upload

---
+++ Interface1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop1
+{
+    public interface IPersonService
+    {
+        public void PersonService();
+    }
+    public interface ICourseService
+    {
+        public void CourseService();
+    }
+    public interface IStudentService : IPersonService
+    {
+
+        public void StudentService();
+
+    }
+    public interface IInstructorService : IPersonService
+    {
+       public void InstrctorService();
+    }
+    public interface IDepartmentService
+    {
+        public void PersonService ();
+    }
+    
+}
---
+++ Program1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace oop1
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        private decimal salary;
+        private string[] address;
+        public void CalculateAge() { }
+        public virtual void CalculateSalary() { }
+        public void getAddress() { }
+    }
+    abstract class Instructor : Person
+    {
+        private string department;
+        private bool head;
+        private DateTime joinDate;
+        public override void CalculateSalary()
+        {
+            //calculate bonus based on joindate
+        }
+    }
+    abstract class Student : Person
+    {
+        private List<string> courseTaken;
+        private Dictionary<string,string> grades;
+        public decimal GPA(Dictionary<string,string> grades)
+        {
+            decimal gpa = 0;
+            //calculation
+            return gpa;
+        } 
+    }
+    class Course
+    {
+        private List<string> enrolledStudent;
+
+    }
+    class Department
+    {
+        private string head;
+        private int budget;
+        private List<string> courses;
+    }
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
